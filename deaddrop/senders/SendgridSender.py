8ee724211a0f6088a17542dbd71f31a5804a35e0
Eric Lan <eric.lan@sendgrid.com>
2015-08-10 15:06:36 -0700

    removed sender classes

--- deaddrop/senders/SendgridSender.py
+++ deaddrop/senders/SendgridSender.py
@@ -1,14 +1,12 @@
 import sendgrid
 from django.conf import settings
 
-class SendgridSender:
+def send(sender, recipient, content):
+	sg = sendgrid.SendGridClient(settings.SENDGRID_API_KEY)
 
-	def send(self, sender, recipient, content):
-		sg = sendgrid.SendGridClient(settings.SENDGRID_API_KEY)
-
-		message = sendgrid.Mail()
-		message.add_to(recipient)
-		message.set_subject("A secret message from %s" % sender)
-		message.set_html(content)
-		message.set_from(sender)
-		status, msg = sg.send(message)
+	message = sendgrid.Mail()
+	message.add_to(recipient)
+	message.set_subject("A secret message from %s" % sender)
+	message.set_html(content)
+	message.set_from(sender)
+	status, msg = sg.send(message)
