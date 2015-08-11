b12677a009c206c56e5c31cebae36892d382fe55
Eric Lan <eric.lan@sendgrid.com>
2015-08-10 23:16:53 -0700

    fixed url for delete

--- deaddrop/assets/js/deaddrop.js
+++ deaddrop/assets/js/deaddrop.js
@@ -76,7 +76,7 @@ $(document).on('click', '#nav-del-msg', function(e) {
 
 function process_delete(){
   var postdata = {"management_key": $('#manage_key')}
-  $.post('http://localhost:8000/api/v1/delete/' + $('#message_uid').val(),
+  $.post('http://localhost:8000/api/v1/delete/' + $('#msg_guid').val(),
     encodeURIComponent(postdata)) 
     .done(function() {
       window.open('https://www.youtube.com/watch?v=fkXGGhuQs0o&t=50');
