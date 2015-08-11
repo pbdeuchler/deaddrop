894aa2be1158d5cabdf6edb7189b3e75d53fa9b7
Philip Deuchler <pbdeuchler@gmail.com>
2015-08-11 10:19:03 -0600

    revert wsgi.py to see if that fixes heroku problems

--- deaddrop/wsgi.py
+++ deaddrop/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 import os
+from os.path import abspath, dirname
+from sys import path
+
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deaddrop.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deaddrop.settings")
 
 application = Cling(get_wsgi_application())
